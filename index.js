6b935d8b7eba75143dfb80058a4ef8051b60ed32
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-06-28 16:44:38 +0200

    fcc  solution file-metadata

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(upload.single('upfile'))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',(req,res)=>{
+  res.json({
+    name:req.file.originalname,
+    type:req.file.mimetype,
+    size:req.file.size,
+  })
+})
 
 
 
